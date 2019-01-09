c1f54fd4a15aa24c2a859acde2589d2c8517249e
Igor Nadiein <snoozzzy7@gmail.com>
2019-01-09 23:52:09 +0200

    init commit

---
+++ server/src/main/java/com/example/mockylikeapp/Mocky.java
@@ -0,0 +1,16 @@
+package com.example.mockylikeapp;
+
+import lombok.*;
+
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Mocky {
+    @Id @GeneratedValue
+    private Long id;
+    private @NonNull String name;
+}
---
+++ server/src/main/java/com/example/mockylikeapp/MockyLikeAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.mockylikeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockyLikeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockyLikeAppApplication.class, args);
+	}
+
+}
+
