0418419bc001260e2d405af4c4129b9012d63da7
Igor <snoozzzy7@gmail.com>
2020-04-28 20:31:42 +0300

    Added copy to clipboard generated text and tooltip

--- client/src/components/MockSinglePage.jsx
+++ client/src/components/MockSinglePage.jsx
@@ -18,7 +18,10 @@ export class MockSinglePage extends React.Component {
         super();
         this.mockService = new MocksService();
         this.state = {
-            mock: new MockModel()
+            mock: new MockModel(),
+            isTooltipVisible: false,
+            posX: 0,
+            posY: 0
         }
     }
 
@@ -30,11 +33,63 @@ export class MockSinglePage extends React.Component {
         })
     }
 
+    onCopyToBufferEvent = e => {
+        let target = e.target;
+        let selection, range;
+        if (document.body.createTextRange) {
+            range = document.body.createTextRange()
+            range.moveToElementText(target)
+            range.select()
+        } else if (window.getSelection) {
+            selection = window.getSelection()
+            range = document.createRange()
+            range.selectNodeContents(target)
+            selection.removeAllRanges()
+            selection.addRange(range)
+        }
+        document.execCommand('copy')
+        selection.removeAllRanges()
+    }
+
+    onDispatchMouseEvent = (e, type) => {
+        switch (type) {
+            case 0: { // event type mouse_over
+                this.setState({ isTooltipVisible: true, posX: e.clientX, posY: e.clientY - 40 })
+                break;
+            }
+            case 1: { // event type mouse_out
+                this.setState({ isTooltipVisible: false })
+                break;
+            }
+            case 2: { // event type mouse_move
+                this.setState({ posX: e.clientX, posY: e.clientY - 40 })
+                break;
+
+            }
+        }
+    }
+
     render() {
+        const { isTooltipVisible } = this.state;
+        let styles = {
+            top: this.state.posY,
+            left: this.state.posX
+        }
         return (
             <div className="mock-single-wrapper">
                 <Link className="btn-back" to={{ pathname: `/mocks` }}><FontAwesomeIcon icon={faChevronLeft} size="xs" /><span>Go back</span></Link>
-                {this.state ? <p className="text-content">{this.state.mock.mockData}</p> : null}
+                { isTooltipVisible ?
+                    <div
+                        ref="tooltip"
+                        style={styles}
+                        className="js-tooltip">Click to copy in buffer</div> : null
+                }
+                {this.state ? <p
+                    className="text-content"
+                    onMouseOver={(e) => this.onDispatchMouseEvent(e, 0)}
+                    onMouseOut={(e) => this.onDispatchMouseEvent(e, 1)}
+                    onMouseMove={(e) => this.onDispatchMouseEvent(e, 2)}
+                    onClick={(e) => this.onCopyToBufferEvent(e)}>{this.state.mock.mockData}</p> : null}
             </div>
         );
     }
